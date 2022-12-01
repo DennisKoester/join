01efb505a3b000510c0a0827561995d813818f8b
TheMo-at <iam@themo.at>
2022-12-01 23:18:40 +0100

    Register.js and Loginform

---
+++ js/register.js
@@ -0,0 +1,11 @@
+
+
+
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+
+    console.log('Username:', username);
+    console.log('Password', password);
+}
