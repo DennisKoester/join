ad24615e56eea4e0ec9f804510beac5bf388286d
TheMo-at <iam@themo.at>
2022-12-02 10:12:10 +0100

    Update Login site to v2

---
+++ js/login.js
@@ -0,0 +1,11 @@
+
+
+
+function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+
+    console.log('Email:', email);
+    console.log('Password', password);
+}
