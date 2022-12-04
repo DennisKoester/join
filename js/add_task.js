1d5f3bf91636c102709a8f69ca9c10faf0592c26
StayAbroad <dennis.koester11@gmail.com>
2022-12-04 22:49:16 +0100

    prio btns finish

--- js/add_task.js
+++ js/add_task.js
@@ -1,19 +1,54 @@
-function redBtn() {
-    const btn = document.getElementById('urgend-btn');
-    let img = document.getElementById('arrows');
+function prioUrgent() {
+    let btn = document.getElementById('urgend-btn');
+    let img = document.getElementById('urgent-btn-img');
     backgroundColor = btn.style.backgroundColor;
-    console.log (backgroundColor, "test");
+    console.log(backgroundColor);
 
-    if (backgroundColor == '#FFFFFF') {
+    if (backgroundColor == 'rgb(255, 255, 255)') {
         btn.style.backgroundColor = '#FF3D00';
         btn.style.color = 'white';
-        img.style.filter = 'brightness(0) invert(1)'
+        img.src = "./assets/img/urgent-white.svg"
     } else {
         btn.style.backgroundColor = '#FFFFFF';
         btn.style.color = 'black';
-        // img.style.filter = 'brightness(0) invert(1)'
+        img.src = "./assets/img/urgent.svg"
     }
+};
 
+
+function prioMedium() {
+    let btn = document.getElementById('medium-btn');
+    let img = document.getElementById('medium-btn-img');
+    backgroundColor = btn.style.backgroundColor;
+    console.log(backgroundColor);
+
+    if (backgroundColor == 'rgb(255, 255, 255)') {
+        btn.style.backgroundColor = '#FFA800';
+        btn.style.color = 'white';
+        img.src = "./assets/img/medium-white.svg"
+    } else {
+        btn.style.backgroundColor = '#FFFFFF';
+        btn.style.color = 'black';
+        img.src = "./assets/img/medium.svg"
+    }
+};
+
+
+function prioLow() {
+    const btn = document.getElementById('low-btn');
+    let img = document.getElementById('low-btn-img');
+    backgroundColor = btn.style.backgroundColor;
+    console.log(backgroundColor);
+
+    if (backgroundColor == 'rgb(255, 255, 255)') {
+        btn.style.backgroundColor = '#7AE229';
+        btn.style.color = 'white';
+        img.src = "./assets/img/low-white.svg"
+    } else {
+        btn.style.backgroundColor = '#FFFFFF';
+        btn.style.color = 'black';
+        img.src = "./assets/img/low.svg"
+    }
 };
 
 
