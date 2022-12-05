18738d806c0023aaf248f9ab82629789c09bb0e2
TheMo-at <iam@themo.at>
2022-12-05 18:57:12 +0100

    Update SignUp
    05/12/22 - 18:57

---
+++ js/sign_up.js
@@ -0,0 +1,11 @@
+
+
+
+function register() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+
+    console.log('Email:', email);
+    console.log('Password', password);
+}
