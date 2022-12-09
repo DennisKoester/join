e222c57a27d3a056dd0e7c59755bcf3e71756918
TheMo-at <iam@themo.at>
2022-12-09 08:12:25 +0100

    preloader first

--- js/login.js
+++ js/login.js
@@ -1,11 +1,33 @@
 
 
+function animation() {
+    var loader = document.getElementById('preloader');
+
+    window.addEventListener('load', function () {
+        loader.style.display = 'none';
+    });
+}
+
 
 function login() {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    
+    console.log(email);
+    console.log(password);
 
+    email = email.trim();
+    password = password.trim();
+    
+    if (email == '' || password == '') {
+        alert('Please fill in all fields');
+        return;
+    }
 
-    console.log('Email:', email);
-    console.log('Password', password);
+    let user = JSON.stringify(email);
+    let pass = JSON.stringify(password);
+
+    localStorage.setItem('user', user);
+    localStorage.setItem('pass', pass);
+
 }
