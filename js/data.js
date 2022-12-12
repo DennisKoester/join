8f8851d5c046be90ef9c892d95f244cd711946f1
colorfultrees <colorfultrees@users.noreply.github.com>
2022-12-12 20:49:10 +0100

    data structure defined

---
+++ js/data.js
@@ -0,0 +1,42 @@
+let users = [
+    {
+        "name": "",
+        "email": "",
+        "password": "",
+        "phone": "",
+        "short_name": "",
+        "color": ""
+    }
+];
+
+let tasks = [
+    [
+        {
+            "title": "",
+            "desc": "",
+            "cat": "",
+            "date": "",
+            "prio": 0,
+            "subtasks": [
+                {
+                    "title": "",
+                    "status": false
+                }
+            ],
+            "assignees": [] // mail-address
+        }
+    ],
+    [],
+    [],
+    []
+];
+
+let category = [];
+
+let prio = [
+    {
+        "name": "",
+        "color": "",
+        "sign": ""
+    }
+];
