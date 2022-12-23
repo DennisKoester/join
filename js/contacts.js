4fe15131ec7bcaad4b8232271f7b296da6769aa9
Marco von Baumbach <marco.vonbaumbach@gmx.de>
2022-12-23 18:31:48 +0100

    comtacts

--- js/contacts.js
+++ js/contacts.js
@@ -20,13 +20,14 @@ function renderContacts() {
 
 function renderContactDivHTML(index) {
     document.getElementById('contacts-menu-scrollable').innerHTML +=  `
-    <div class="contact-div">
+    <div id="contact-container${index}" class="contact-div" onclick="renderContactInformation(${index})">
         <div class="username-initials" id="username-initials${index}"></div>
         <div>
             <div id="users-name${index}">Hans Test</div>
             <a class="users-email" id="users-email${index}">hans@test.de</a>
         </div>
-    </div>`;
+    </div>
+    `;
 
     insertUserInformationById(index);
 }
@@ -39,3 +40,44 @@ function insertUserInformationById(index) {
     document.getElementById(`users-email${index}`).href = 'mailto:' + users[index]['email'];
 }
 
+function renderContactInformation(index) {
+    document.getElementById('contact-div').innerHTML = `
+        <div class="contact-div-big">
+        <div class="username-initials-big" id="u-initials${index}">JM</div>
+            <div>
+                <div class="username-big" id="u-name${index}">Hans Test</div>
+                <img src="./assets/img/add_task.png">
+            </div>
+        </div>
+        <div class="contact-and-edit">
+            <div class="contact-information">Contact Information</div>
+            <div class="edit-contact"><img src="./assets/img/edit_contact.png"><span class="edit-contact-text">Edit Contact</span></div>
+        </div>
+        <div class="email-margins"><b>Email</b></div>
+        <a class="users-email-big" id="u-email${index}">testmail</a>
+        <div class="phone-margins"><b>Phone</b></div>
+        <div id="u-phone-number${index}">no phone number</div>
+    `;
+    renderContactInformationById(index);
+    backgroundColorOfSelected(index);
+}
+
+function renderContactInformationById(index) {
+    document.getElementById(`u-initials${index}`).innerHTML = users[index]['short_name'];
+    document.getElementById(`u-initials${index}`).style.backgroundColor = users[index]['color'];
+    document.getElementById(`u-name${index}`).innerHTML = users[index]['name'];
+    document.getElementById(`u-email${index}`).innerHTML = users[index]['email'];
+    document.getElementById(`u-email${index}`).href = 'mailto:' + users[index]['email'];
+    if(users[index]['phone']){
+        document.getElementById(`u-phone-number${index}`).innerHTML = users[index]['phone'];
+    }
+}
+
+function backgroundColorOfSelected(index) {
+    for(let i = 0; i < users.length; i++) {
+        if (document.getElementById(`contact-container${i}`).classList.contains('contacts-selected')){
+            document.getElementById(`contact-container${i}`).classList.remove('contacts-selected');
+        }
+    }
+    document.getElementById(`contact-container${index}`).classList.add('contacts-selected');
+}
